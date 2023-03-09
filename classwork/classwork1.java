6f1e41df4e3ee17e69705b240deef95ea456d66d
Gooocha <113640428+Gooocha@users.noreply.github.com>
2023-03-09 15:44:55 +0300

    Create classwork1.java

---
+++ classwork/classwork1.java
@@ -0,0 +1,59 @@
+/*
+package classwork;
+
+import java.util.Scanner;
+
+public class classwork1 {
+ /*
+    static public void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int count = 1;
+        while (count * count <= a){
+            System.out.println(count*count);
+            count +=1 ;
+        }
+    }
+
+ static public void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+ /*  int a = scan.nextInt();
+     int i = 0 ;
+     int c = 0;
+     while (a != 0 ){
+         c = c + a ;
+         i= i + 1;
+         a = scan.nextInt();
+     }
+    System.out.println(c / i);
+ }
+
+     int a = scan.nextInt();
+     int b = scan.nextInt();
+     int i = 0;
+     while ( a <= b){
+         a++;
+         if (a % 3 == 0 && a % 5 != 0  )
+         {
+             i++;
+         }
+     }
+     System.out.println(i);
+ }
+}
+*/
+package classwork;
+
+import java.util.Scanner;
+
+public class classwork1 {
+    static public void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+
+
+
+
+
+    }
+}
